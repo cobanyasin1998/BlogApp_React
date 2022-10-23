65ae3a7f1197ae550e289e6859b1e047d9725a91
Yasin Çoban <yasincoban1998@outlook.com>
2022-10-23 14:37:49 +0300

    redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AppRouter from './routers/AppRouter'
+import AppRouter from "./routers/AppRouter";
 import "./styles/main.scss";
 
+import { createStore } from "redux";
+
+const initialState = {
+  count: 0,
+};
+const store = createStore((state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
+      return {
+        count: state.count + incrementBy,
+      };
+
+    case "DECREMENT":
+      return {
+        count: state.count - 1,
+      };
+
+    default:
+      return state;
+  }
+  return state;
+});
+
+console.log(store.getState());
+
+store.dispatch({
+  type: "INCREMENT",
+  incrementBy: 10,
+});
+store.dispatch({
+  type: "INCREMENT",
+});
+store.dispatch({
+  type: "DECREMENT",
+});
+
+store.subcribe(()=>{
+
+    console.log(store.getState());
+
+})
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
